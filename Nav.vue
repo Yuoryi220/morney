396e34acaefd9d914e306d54568372f3ed413419
Yuoryi220 <80259994+Yuoryi220@users.noreply.github.com>
2021-08-26 16:06:51 +0800

    Add files via upload

---
+++ Nav.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="nav">
+    <router-link to="/money">记账</router-link>
+
+    <router-link to="/labels">
+      <svg>
+         <use xlink:href="#label"/>
+      </svg>
+      标签
+    </router-link>
+
+    <router-link to="/statistics">统计</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import x from "@/assets/icons/label.svg"
+
+export default {
+  name: "Nav"
+}
+</script>
+
+<style lang="scss" scoped>
+.nav{
+  border:1px solid red;
+}
+</style>
